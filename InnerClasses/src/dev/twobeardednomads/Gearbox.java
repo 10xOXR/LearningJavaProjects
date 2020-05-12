701173d49c8fa8903aac46e3f3e67e3eb420f1bc
10xOXR <41506849+10xOXR@users.noreply.github.com>
2020-05-12 11:34:50 +0100

    Change Gear class scope and add additional Gearbox methods

--- InnerClasses/src/dev/twobeardednomads/Gearbox.java
+++ InnerClasses/src/dev/twobeardednomads/Gearbox.java
@@ -7,6 +7,7 @@ public class Gearbox {
     private ArrayList<Gear> gears;
     private int maxGears;
     private int currentGear = 0;
+    private boolean clutchIsIn;
 
     public Gearbox(int maxGears) {
         this.maxGears = maxGears;
@@ -15,13 +16,45 @@ public class Gearbox {
         this.gears.add(neutral);
     }
 
-    public class Gear {
+    public void operateClutch(boolean in) {
+        this.clutchIsIn = in;
+    }
+
+    public void addGear(int number, double ratio) {
+        if ((number > 0) && (number <= maxGears) && (ratio >= 0.0)) {
+            this.gears.add(new Gear(number, ratio));
+        }
+    }
+
+    public void changeGear(int newGear) {
+        if ((newGear >= 0) && (newGear < this.gears.size()) && this.clutchIsIn) {
+            this.currentGear = newGear;
+            System.out.println("Gear " + newGear + " selected.");
+        } else {
+            System.out.println("Grind!");
+            this.currentGear = 0;
+        }
+    }
+
+    public double wheelSpeed (int revs) {
+        if (clutchIsIn) {
+            System.out.println("Scream!");
+            return 0.0;
+        }
+        return revs * gears.get(currentGear).getRatio();
+    }
+
+    private class Gear {
         private int gearNumber;
         private double ratio;
 
         public Gear(int gearNumber, double ratio) {
             this.gearNumber = gearNumber;
             this.ratio = ratio;
+        }
+
+        public double getRatio() {
+            return ratio;
         }
 
         public double driveSpeed(int revs) {
