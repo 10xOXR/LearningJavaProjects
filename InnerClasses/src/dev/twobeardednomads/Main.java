8f7f75a6f54d3f3359d46bb5491e43fb1daf7e54
10xOXR <41506849+10xOXR@users.noreply.github.com>
2020-05-12 11:56:43 +0100

    Make 'addGear' part of the Gearbox constructor

--- InnerClasses/src/dev/twobeardednomads/Main.java
+++ InnerClasses/src/dev/twobeardednomads/Main.java
@@ -4,7 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
         Gearbox mcLaren = new Gearbox(6);
-        Gearbox.Gear first = mcLaren.new Gear(1, 12.3);
-        System.out.println(first.driveSpeed(1000));
+        mcLaren.operateClutch(true);
+        mcLaren.changeGear(1);
+        mcLaren.operateClutch(false);
+        System.out.println(mcLaren.wheelSpeed(1000));
+        mcLaren.changeGear(2);
+        System.out.println(mcLaren.wheelSpeed(3000));
+        mcLaren.operateClutch(true);
+        mcLaren.changeGear(3);
+        mcLaren.operateClutch(false);
+        System.out.println(mcLaren.wheelSpeed(6000));
     }
 }
