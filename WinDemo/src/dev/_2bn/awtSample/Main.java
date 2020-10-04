c572125ec14b3d50e390549d2c9a6785348e9a21
10xOXR <41506849+10xOXR@users.noreply.github.com>
2020-10-04 13:28:51 +0100

    Add AWT window demo code

---
+++ WinDemo/src/dev/_2bn/awtSample/Main.java
@@ -0,0 +1,9 @@
+package dev._2bn.awtSample;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    MyWindow newWindow = new MyWindow("Hello World");
+	    newWindow.setVisible(true);
+    }
+}
---
+++ WinDemo/src/dev/_2bn/awtSample/MyWindow.java
@@ -0,0 +1,30 @@
+package dev._2bn.awtSample;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class MyWindow extends Frame {
+
+    public MyWindow(String title) {
+        super(title);
+        setSize(350, 140);
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        Font sansSerifLarge = new Font("SansSerif", Font.BOLD, 18);
+        Font sansSerifSmall = new Font("SansSerif", Font.BOLD, 12);
+        g.setFont(sansSerifLarge);
+        g.drawString("Hello World (in a Window)", 60, 60);
+        g.setFont(sansSerifSmall);
+        g.drawString("by Chris Quinn", 60, 100);
+    }
+}
